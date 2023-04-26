87e75a54c689848e43bb79711d8f87ca82c62fe4
Cassie Hernandez <cassie@Cassies-MacBook-Air.local>
2023-04-25 20:17:52 -0700

    authentication

--- Healthy-Minds-Backend/CoreRoot/settings.py
+++ Healthy-Minds-Backend/CoreRoot/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'core',
     'rest_framework',
     'core.user',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -43,6 +44,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -137,3 +140,8 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
     )
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
+]
--- Healthy-Minds-Backend/core/user/viewsets.py
+++ Healthy-Minds-Backend/core/user/viewsets.py
@@ -9,18 +9,22 @@ class UserViewSet(viewsets.ModelViewSet):
     http_method_names = ['get']
     serializer_class = UserSerializer
     permission_classes = (IsAuthenticated,)
-    filter_backends = [filters.OrderingFilter]
-    ordering_fields = ['updated']
-    ordering = ['-updated']
+    # filter_backends = [filters.OrderingFilter]
+    # ordering_fields = ['updated']
+    # ordering = ['-updated']
 
     def get_queryset(self):
         if self.request.user.is_superuser:
             return User.objects.all()
 
     def get_object(self):
+        print(self.kwargs)
+        print(self.lookup_field)
         lookup_field_value = self.kwargs[self.lookup_field]
 
-        obj = User.objects.get(lookup_field_value)
+        print("i made it here")
+        obj = User.objects.get(id=lookup_field_value)
         self.check_object_permissions(self.request, obj)
+
 
         return obj
